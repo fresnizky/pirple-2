1ab73ea377a28bf1064b8b60fdf19eb90fe8429a
Federico Resnizky <federico.resnizky@intive-fdv.com>
2018-11-25 00:25:44 -0300

    Added menu handler Refactored handlers to separate files

--- doc/api_project.js
+++ doc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-11-24T17:58:30.909Z",
+    "time": "2018-11-24T22:40:07.123Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
